f6b492aecfbc818d907e3f64f9c26e0a4c9d8e56
Wafatriki <azdadwafa0@gmail.com>
2025-03-22 14:37:07 +0000

    login terminado

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -61,6 +61,10 @@ export default function Layout({ children }) {
             <FontAwesome5 name="car" size={24} color={color} ></FontAwesome5>
             <Text style={styles.menuItem}>Explora</Text>
           </Link>
+          <Link style={styles.menuItem} href="login">
+            <FontAwesome name="sign-in" size={24} color={color} />
+            <Text style={styles.menuItem}>Iniciar Sesión</Text>
+          </Link>
           <Text style={styles.menuItem}>Configuración</Text>
         </View>
       )}

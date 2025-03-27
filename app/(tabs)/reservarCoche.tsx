7d5ab110d685abf49f1722f5f80851e379fc7a18
Nicogameps5 <nicolashernandezcastro08@gmail.com>
2025-03-27 16:48:04 +0000

    FIX reservarCoches v2

--- app/(tabs)/reservarCoche.tsx
+++ app/(tabs)/reservarCoche.tsx
@@ -217,7 +217,7 @@ const styles = StyleSheet.create({
     marginRight: 10,
     color: theme.colors.text,
     fontFamily: theme.fonts.regular,
-    flex: 1;
+    flex: 1,
   },
   total: {
     fontSize: 24,

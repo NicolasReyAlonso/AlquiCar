92e4364a7c0a13a1615f242252c74da2d22143c8
Nicolás Rey Alonso <NicolasReyAlonso@gmail.com>
2025-04-04 12:40:22 +0100

    ADD get by email

--- alquicar-backend/controllers/users.js
+++ alquicar-backend/controllers/users.js
@@ -20,6 +20,23 @@ export class UserController {
         }
 
     }
+    getByEmail = async (req, res) => {
+        const { email } = req.params;
+        try{
+            const users = await this.userModel.getByEmail({ email })
+            if (users.length === 0) {
+                return res.status(404).json({ error: 'User not found' });
+            }
+            return res.json(users);
+        }catch{
+            if (e instanceof DatabaseError) {
+                return res.status(500).json({ error: e.message });
+            }
+
+            console.log(e);
+        }
+        
+    }
 
     getById = async (req, res) => {
         const { id } = req.params;
@@ -43,6 +60,8 @@ export class UserController {
         }
 
     }
+
+
 
     create = async (req, res) => {
 

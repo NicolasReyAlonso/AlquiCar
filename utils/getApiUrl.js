03b76b0abffd025ac2c1b07031da538b6641206f
Wafatriki <azdadwafa0@gmail.com>
2025-05-20 12:06:11 +0100

    ,

--- utils/getApiUrl.js
+++ utils/getApiUrl.js
@@ -4,11 +4,7 @@ export const getApiUrl = () => {
     if(Platform.OS === "web") {
         return "http://localhost:3000"
     } else {
-<<<<<<< HEAD
-        return "http://10.193.139.24:3000"
-=======
-        return "http://192.168.1.42:3000"
->>>>>>> 1920fbd2385b47115dded084bf3d5ec64b90b80c
+        return "http://10.195.14.247:3000"
     }
 
 }
@@ -16,6 +12,6 @@ export const getAppUrl = () => {
     if(Platform.OS === "web") {
         return "http://localhost:8081"
     } else {
-        return "http://192.168.1.42:8081"
+        return "http://10.195.14.247:8081"
     }
 }

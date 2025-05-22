7e8e2aeb7662d02edd32882b2bdda5e3923674b3
Nicogameps5 <nicolashernandezcastro08@gmail.com>
2025-05-22 12:53:57 +0100

    correción

--- app/(tabs)/account.tsx
+++ app/(tabs)/account.tsx
@@ -26,11 +26,15 @@ export default function AccountPage() {
 
   useEffect(() => {
     const fetchProfileImage = async () => {
-      const response = await fetch(`http://${getApiUrl()}/media/profile/${userId}`);
-      const data = await response.json();
+      try {
+        const response = await fetch(`http://${getApiUrl()}/media/profile/${userId}`);
+        const data = await response.json();
 
-      if (data.length > 0) {
-        setProfileImageUri(data[0].data);
+        if (data.length > 0) {
+          setProfileImageUri(data[0].data);
+        }
+      } catch (error) {
+        console.error("Error al cargar imagen de perfil:", error);
       }
     };
 
